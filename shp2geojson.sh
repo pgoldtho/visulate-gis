456f958718edab85d75bfc35add7820284ab8402
pgoldtho <peter@goldthorp.com>
2018-10-10 15:02:56 -0400

    2011 B* Counties

--- shp2geojson.sh
+++ shp2geojson.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 c=0
-for i in ./shapefiles/a*.shp; do
+for i in ./shapefiles/b*.shp; do
   t=`echo $i |sed s/shapefiles/geojsonfiles/ |sed s/_2018pin// | sed s/_2018// | sed s/_pin//`
   target=`echo $t | sed s/.shp/.geojson/`
   echo ogr2ogr -f GeoJSON -t_srs crs:84 $target $i
